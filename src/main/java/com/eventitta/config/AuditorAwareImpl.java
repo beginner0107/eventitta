231a85f75166a22c4fc0c2fc3e1554584c3a8faa
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-05-06 19:01:54 +0900

    feat: Spring Data JPA auditing 설정 추가
    - 보안 컨텍스트에서 현재 사용자 이름을 추출하기 위해 AuditorAwareImpl을 구현
    - 생성된 날짜, 생성된 날짜, 마지막 수정 날짜, 마지막 수정 날짜 필드가 있는 베이스 엔티티 매핑된 슈퍼클래스를 생성
    - JPA 감사를 활성화하고 AuditorAware 빈을 등록하기 위해 JpaAuditingConfig를 추가

---
+++ src/main/java/com/eventitta/config/AuditorAwareImpl.java
@@ -0,0 +1,19 @@
+package com.eventitta.config;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null || !auth.isAuthenticated() || auth.getPrincipal().equals("anonymousUser")) {
+            return Optional.empty();
+        }
+        return Optional.of(auth.getName());
+    }
+}

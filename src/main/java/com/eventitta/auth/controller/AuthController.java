cf869cd5cb0a72c712b92fb29f0174315e6846fe
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-05-07 18:27:17 +0900

    feat: 회원가입 api 스웨거 작성

--- src/main/java/com/eventitta/auth/controller/AuthController.java
+++ src/main/java/com/eventitta/auth/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.eventitta.auth.controller;
 import com.eventitta.auth.dto.request.SignUpRequest;
 import com.eventitta.auth.dto.response.SignUpResponse;
 import com.eventitta.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,10 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
+@Tag(name = "인증 API", description = "회원가입, 로그인 등의 인증 관련 API")
 public class AuthController {
     private final AuthService authService;
     private final PasswordEncoder passwordEncoder;
 
+    @Operation(summary = "회원가입", description = "회원 정보를 받아 회원가입을 수행합니다.")
     @PostMapping("/signup")
     public SignUpResponse signUp(@Valid @RequestBody SignUpRequest req) {
         var user = authService.signUp(req.toEntity(passwordEncoder));

a14385d2b6fd6046321f129df409f5094f7e9583
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-05-10 20:36:20 +0900

    refactor: 로그인 dto 유효성 검증 로직 추가

--- src/main/java/com/eventitta/auth/dto/request/SignInRequest.java
+++ src/main/java/com/eventitta/auth/dto/request/SignInRequest.java
@@ -1,7 +1,16 @@
 package com.eventitta.auth.dto.request;
 
+import com.eventitta.common.constants.RegexPattern;
+import com.eventitta.common.constants.ValidationMessage;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record SignInRequest(
+    @NotBlank(message = ValidationMessage.EMAIL)
+    @Pattern(regexp = RegexPattern.EMAIL, message = ValidationMessage.EMAIL)
     String email,
+    @NotBlank(message = ValidationMessage.PASSWORD)
+    @Pattern(regexp = RegexPattern.PASSWORD, message = ValidationMessage.PASSWORD)
     String password
 ) {
 }

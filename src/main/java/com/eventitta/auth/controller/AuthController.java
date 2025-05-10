80bc2c4affd4427f538dd1b8d6d5cf523b290f11
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-05-10 14:05:17 +0900

    refactor: service layer에서 dto를 받아 처리하도록 변경
    서비스는 도메인 로직 + dto 변환 책임을 포함하도록 분리

--- src/main/java/com/eventitta/auth/controller/AuthController.java
+++ src/main/java/com/eventitta/auth/controller/AuthController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "인증 API", description = "회원가입, 로그인 등의 인증 관련 API")
 public class AuthController {
     private final AuthService authService;
-    private final PasswordEncoder passwordEncoder;
 
     @Operation(summary = "회원가입", description = "회원 정보를 받아 회원가입을 수행합니다.")
     @PostMapping("/signup")
     public ResponseEntity<SignUpResponse> signUp(@Valid @RequestBody SignUpRequest req) {
-        var user = authService.signUp(req.toEntity(passwordEncoder));
+        var user = authService.signUp(req);
         return ResponseEntity.ok(SignUpResponse.of(user));
     }
 }
--- src/main/java/com/eventitta/auth/service/AuthService.java
+++ src/main/java/com/eventitta/auth/service/AuthService.java
@@ -1,10 +1,12 @@
 package com.eventitta.auth.service;
 
+import com.eventitta.auth.dto.request.SignUpRequest;
 import com.eventitta.auth.exception.SignUpErrorCode;
 import com.eventitta.user.domain.User;
 import com.eventitta.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,14 +14,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public User signUp(User user) {
-        if (userRepository.existsByEmail(user.getEmail())) {
+    public User signUp(SignUpRequest request) {
+        if (userRepository.existsByEmail(request.email())) {
             throw SignUpErrorCode.CONFLICTED_EMAIL.defaultException();
         }
-        if (userRepository.existsByNickname(user.getNickname())) {
+        if (userRepository.existsByNickname(request.nickname())) {
             throw SignUpErrorCode.CONFLICTED_NICKNAME.defaultException();
         }
-        return userRepository.save(user);
+        return userRepository.save(request.toEntity(passwordEncoder));
     }
 }

57f299b328de9777ccaba6787e791e550602d4a1
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-05-10 20:10:01 +0900

    test: AuthController 의존성이 추가되어 슬라이스 테스트에 @MockitoBean 추가
    JwtTokenProvider, CustomUserDetailsService

--- src/test/java/com/eventitta/ControllerTestSupport.java
+++ src/test/java/com/eventitta/ControllerTestSupport.java
@@ -1,7 +1,9 @@
 package com.eventitta;
 
 import com.eventitta.auth.controller.AuthController;
+import com.eventitta.auth.jwt.JwtTokenProvider;
 import com.eventitta.auth.service.AuthService;
+import com.eventitta.auth.service.CustomUserDetailsService;
 import com.eventitta.common.config.SecurityConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +23,8 @@ public abstract class ControllerTestSupport {
     protected ObjectMapper objectMapper;
     @MockitoBean
     protected AuthService authService;
+    @MockitoBean
+    protected JwtTokenProvider jwtTokenProvider;
+    @MockitoBean
+    protected CustomUserDetailsService customUserDetailsService;
 }

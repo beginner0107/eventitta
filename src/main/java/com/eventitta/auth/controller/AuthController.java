ddf9cfae1f8c2c38e95653b6ab1b0fba2f498041
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-05-11 00:29:31 +0900

    docs: login, refresh 메서드 Swagger 문서 주석 추가
    cookie 내려주기 위해 두 가지 설정 추가
    - with-credentials: true: 쿠키를 함께 전송
    - persist-authorization: true: 새로고침해도 인증정보 유지

--- src/main/java/com/eventitta/auth/controller/AuthController.java
+++ src/main/java/com/eventitta/auth/controller/AuthController.java
@@ -3,11 +3,11 @@ package com.eventitta.auth.controller;
 import com.eventitta.auth.dto.request.SignInRequest;
 import com.eventitta.auth.dto.request.SignUpRequest;
 import com.eventitta.auth.dto.response.SignUpResponse;
-import com.eventitta.auth.dto.response.TokenResponse;
-import com.eventitta.auth.jwt.JwtTokenProvider;
 import com.eventitta.auth.service.AuthService;
-import com.eventitta.common.util.CookieUtil;
+import com.eventitta.auth.service.SignUpService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -21,30 +21,35 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "인증 API", description = "회원가입, 로그인 등의 인증 관련 API")
 public class AuthController {
     private final AuthService authService;
-    private final JwtTokenProvider jwtTokenProvider;
 
     @Operation(summary = "회원가입", description = "회원 정보를 받아 회원가입을 수행합니다.")
     @PostMapping("/signup")
-    public ResponseEntity<SignUpResponse> signUp(@Valid @RequestBody SignUpRequest req) {
-        var user = authService.signUp(req);
+    public ResponseEntity<SignUpResponse> signUp(@Valid @RequestBody SignUpRequest request) {
+        var user = authService.signUp(request);
         return ResponseEntity.ok(SignUpResponse.of(user));
     }
 
+    @Operation(summary = "로그인", description = "회원 정보를 받아 로그인을 수행합니다.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", ref = "#/components/responses/TokensSetCookies"),
+    })
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@Valid @RequestBody SignInRequest req, HttpServletResponse resp) {
-        TokenResponse tokenResponse = authService.login(req.email(), req.password());
-        CookieUtil.addTokenCookies(resp, tokenResponse, jwtTokenProvider);
+    public ResponseEntity<Void> login(@Valid @RequestBody SignInRequest request, HttpServletResponse response) {
+        authService.login(request, response);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "토큰 재발급", description = "엑세스, 리프레시 토큰을 받아 토큰을 재발급합니다.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", ref = "#/components/responses/TokensSetCookies"),
+    })
     @PostMapping("/refresh")
     public ResponseEntity<Void> refresh(
-        @CookieValue(name = "access_token", required = false) String at,
-        @CookieValue(name = "refresh_token", required = false) String rt,
-        HttpServletResponse resp
+        @CookieValue(name = "access_token", required = false) String accessToken,
+        @CookieValue(name = "refresh_token", required = false) String refreshToken,
+        HttpServletResponse response
     ) {
-        TokenResponse tokens = authService.refreshTokens(at, rt);
-        CookieUtil.addTokenCookies(resp, tokens, jwtTokenProvider);
+        authService.refresh(accessToken, refreshToken, response);
         return ResponseEntity.ok().build();
     }
 }

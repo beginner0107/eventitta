0391ee84511f6758cfbc9b0540cfbe0bd0853eec
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-05-18 01:17:25 +0900

    feat: 지역 기반 게시글 등록 api 구현

--- src/main/java/com/eventitta/post/controller/PostController.java
+++ src/main/java/com/eventitta/post/controller/PostController.java
@@ -1,9 +1,38 @@
 package com.eventitta.post.controller;
 
+import com.eventitta.auth.annotation.CurrentUser;
+import com.eventitta.auth.domain.UserPrincipal;
+import com.eventitta.post.dto.request.CreatePostRequest;
+import com.eventitta.post.dto.response.CreatePostResponse;
+import com.eventitta.post.service.PostService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @Slf4j
+@RequestMapping("/api/v1/posts")
 @RestController
+@RequiredArgsConstructor
 public class PostController {
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<CreatePostResponse> create(
+        @CurrentUser Long userId,
+        @RequestBody @Valid CreatePostRequest request
+    ) {
+        Long postId = postService.create(userId, request);
+
+        return ResponseEntity
+            .created(URI.create("/api/posts/" + postId))
+            .body(new CreatePostResponse(postId));
+    }
 }
--- src/main/java/com/eventitta/post/service/PostService.java
+++ src/main/java/com/eventitta/post/service/PostService.java
@@ -1,9 +1,39 @@
 package com.eventitta.post.service;
 
+import com.eventitta.post.domain.Post;
+import com.eventitta.region.domain.Region;
+import com.eventitta.post.dto.request.CreatePostRequest;
+import com.eventitta.post.repository.PostRepository;
+import com.eventitta.region.repository.RegionRepository;
+import com.eventitta.user.domain.User;
+import com.eventitta.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.eventitta.region.exception.RegionErrorCode.NOT_FOUND_REGION_CODE;
+import static com.eventitta.user.exception.UserErrorCode.NOT_FOUND_USER_ID;
+
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class PostService {
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final RegionRepository regionRepository;
+
+    public Long create(Long userId, CreatePostRequest dto) {
+        User user = userRepository.findById(userId)
+            .orElseThrow(NOT_FOUND_USER_ID::defaultException);
+        Region region = regionRepository.findById(dto.regionCode())
+            .orElseThrow(NOT_FOUND_REGION_CODE::defaultException);
+
+        Post post = Post.create(
+            user,
+            dto.title(),
+            dto.content(),
+            region
+        );
+        return postRepository.save(post).getId();
+    }
 }

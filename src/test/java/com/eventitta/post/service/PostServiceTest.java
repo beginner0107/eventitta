a6fe80642735702e0b71ef1ef37ea59c1b77d7d3
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-05-18 22:17:52 +0900

    test(post): 게시글 수정에 대한 비즈니스 로직 테스트 코드 작성
    - 성공(1) / 실패(3)
    1. 수정 성공
    2. 수정 실패(존재하지 않는 게시글, 본인이 작성한 글이 아닌 경우, 유효하지 않은 지역 코드)

--- src/test/java/com/eventitta/post/service/PostServiceTest.java
+++ src/test/java/com/eventitta/post/service/PostServiceTest.java
@@ -2,6 +2,9 @@ package com.eventitta.post.service;
 
 import com.eventitta.post.domain.Post;
 import com.eventitta.post.dto.request.CreatePostRequest;
+import com.eventitta.post.dto.request.UpdatePostRequest;
+import com.eventitta.post.exception.PostErrorCode;
+import com.eventitta.post.exception.PostException;
 import com.eventitta.post.repository.PostRepository;
 import com.eventitta.region.domain.Region;
 import com.eventitta.region.exception.RegionErrorCode;
@@ -26,6 +29,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class PostServiceTest {
@@ -39,6 +44,9 @@ class PostServiceTest {
 
     @InjectMocks
     PostService postService;
+
+    private final String VALID_REGION = "1100110100";
+    private final String INVALID_REGION = "0000000000";
 
     @Test
     @DisplayName("유효한 요청으로 생성 시 새 게시글 ID가 반환된다")
@@ -96,6 +104,95 @@ class PostServiceTest {
             .isEqualTo(RegionErrorCode.NOT_FOUND_REGION_CODE);
     }
 
+    @Test
+    @DisplayName("업데이트시 기존 게시물 및 소유자 및 유효한 지역이 주어지면 제목, 콘텐츠, 지역이 변경됩니다.")
+    void givenPostAndOwnerAndValidRegion_whenUpdate_thenFieldsUpdated() {
+        // given
+        final long POST_ID = 1L;
+        final long USER_ID = 10L;
+        Region region = createRegion(VALID_REGION);
+        Post post = spy(Post.create(
+            createUser(USER_ID, "test@test.com", "pw123123", "유저"),
+            "oldTitle", "oldContent",
+            region
+        ));
+        given(postRepository.findById(POST_ID))
+            .willReturn(Optional.of(post));
+        given(regionRepository.findById(VALID_REGION))
+            .willReturn(Optional.of(region));
+
+        UpdatePostRequest dto = createUpdateDto("newTitle", "newContent", VALID_REGION);
+
+        // when
+        postService.update(POST_ID, USER_ID, dto);
+
+        // then
+        verify(post).update("newTitle", "newContent", region);
+        assertThat(post.getTitle()).isEqualTo("newTitle");
+        assertThat(post.getContent()).isEqualTo("newContent");
+        assertThat(post.getRegion().getCode()).isEqualTo(VALID_REGION);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 게시글을 수정 시 예외가 반환된다.")
+    void givenNoPost_whenUpdate_thenThrowNotFound() {
+        // given
+        final long POST_ID = 1L;
+        final long USER_ID = 10L;
+        given(postRepository.findById(POST_ID))
+            .willReturn(Optional.empty());
+
+        // when / then
+        assertThatThrownBy(() ->
+            postService.update(POST_ID, USER_ID, createUpdateDto("t", "c", VALID_REGION))
+        )
+            .isInstanceOf(PostException.class)
+            .extracting("errorCode")
+            .isEqualTo(PostErrorCode.NOT_FOUND_POST_ID);
+    }
+
+
+    @Test
+    @DisplayName("게시글의 소유자와 수정자가 다른 사용자이면 예외가 발생하는다.")
+    void givenPostOfOtherUser_whenUpdate_thenThrowAccessDenied() {
+        // given
+        final long POST_ID = 1L;
+        final long USER_ID = 10L;
+        final long OTHER_USER_ID = 20L;
+        User user = createUser(USER_ID, "test@test.com", "pw123123", "유저");
+        Post post = createPost(POST_ID, user, "title", createRegion(VALID_REGION));
+        given(postRepository.findById(POST_ID)).willReturn(Optional.of(post));
+
+        // when & then
+        assertThatThrownBy(() ->
+            postService.update(POST_ID, OTHER_USER_ID, createUpdateDto("t", "c", VALID_REGION))
+        )
+            .isInstanceOf(PostException.class)
+            .extracting("errorCode")
+            .isEqualTo(PostErrorCode.ACCESS_DENIED);
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 지역 코드로 게시글을 수정 시 예외가 발생하는다.")
+    void givenInvalidRegion_whenUpdate_thenThrowRegionNotFound() {
+        // given
+        final long POST_ID = 1L;
+        final long USER_ID = 10L;
+        User user = createUser(USER_ID, "test@test.com", "pw123123", "유저");
+        Post post = createPost(POST_ID, user, "title", createRegion(INVALID_REGION));
+        given(postRepository.findById(POST_ID)).willReturn(Optional.of(post));
+
+        given(regionRepository.findById(INVALID_REGION)).willReturn(Optional.empty());
+
+        // when / then
+        assertThatThrownBy(() ->
+            postService.update(POST_ID, USER_ID, createUpdateDto("t", "c", INVALID_REGION))
+        )
+            .isInstanceOf(RegionException.class)
+            .extracting("errorCode")
+            .isEqualTo(RegionErrorCode.NOT_FOUND_REGION_CODE);
+    }
+
     private static User createUser(long userId, String email, String password, String nickname) {
         return User.builder()
             .id(userId)
@@ -116,12 +213,16 @@ class PostServiceTest {
             .build();
     }
 
-    private static Post createPost(Long userId, User user, String title, Region region) {
+    private static Post createPost(Long postId, User user, String title, Region region) {
         return Post.builder()
-            .id(userId)
+            .id(postId)
             .user(user)
             .title(title)
             .region(region)
             .build();
+    }
+
+    private UpdatePostRequest createUpdateDto(String title, String content, String regionCode) {
+        return new UpdatePostRequest(title, content, regionCode);
     }
 }

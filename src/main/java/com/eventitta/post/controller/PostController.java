5ef48fa056d545cf896da4092215c5394c158cb0
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-05-18 22:14:17 +0900

    feat(post): 게시글 수정 api, 서비스 로직 작성, 예외 처리

--- src/main/java/com/eventitta/post/controller/PostController.java
+++ src/main/java/com/eventitta/post/controller/PostController.java
@@ -2,18 +2,18 @@ package com.eventitta.post.controller;
 
 import com.eventitta.auth.annotation.CurrentUser;
 import com.eventitta.post.dto.request.CreatePostRequest;
+import com.eventitta.post.dto.request.UpdatePostRequest;
 import com.eventitta.post.dto.response.CreatePostResponse;
 import com.eventitta.post.service.PostService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -23,13 +23,15 @@ import java.net.URI;
 @Tag(name = "지역기반 커뮤니티 API", description = "커뮤니티 게시글 API")
 @RequiredArgsConstructor
 public class PostController {
+
     private final PostService postService;
 
-
     @Operation(
-        summary = "게시글 생성",
-        description = "제목, 내용, 지역 코드로 새 게시글을 생성합니다."
+        summary = "게시글 생성"
     )
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "게시글 등록 성공"),
+    })
     @PostMapping
     public ResponseEntity<CreatePostResponse> create(
         @CurrentUser Long userId,
@@ -39,5 +41,21 @@ public class PostController {
         return ResponseEntity
             .created(URI.create("/api/v1/posts/" + postId))
             .body(new CreatePostResponse(postId));
+    }
+
+    @Operation(
+        summary = "게시글 수정"
+    )
+    @ApiResponses({
+        @ApiResponse(responseCode = "204", description = "게시글 수정 성공"),
+    })
+    @PutMapping("/{postId}")
+    public ResponseEntity<Void> update(
+        @PathVariable("postId") Long postId,
+        @CurrentUser Long userId,
+        @RequestBody @Valid UpdatePostRequest request
+    ) {
+        postService.update(postId, userId, request);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/eventitta/post/service/PostService.java
+++ src/main/java/com/eventitta/post/service/PostService.java
@@ -1,9 +1,11 @@
 package com.eventitta.post.service;
 
 import com.eventitta.post.domain.Post;
-import com.eventitta.region.domain.Region;
 import com.eventitta.post.dto.request.CreatePostRequest;
+import com.eventitta.post.dto.request.UpdatePostRequest;
 import com.eventitta.post.repository.PostRepository;
+import com.eventitta.region.domain.Region;
+import com.eventitta.region.exception.RegionErrorCode;
 import com.eventitta.region.repository.RegionRepository;
 import com.eventitta.user.domain.User;
 import com.eventitta.user.repository.UserRepository;
@@ -11,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.eventitta.post.exception.PostErrorCode.ACCESS_DENIED;
+import static com.eventitta.post.exception.PostErrorCode.NOT_FOUND_POST_ID;
 import static com.eventitta.region.exception.RegionErrorCode.NOT_FOUND_REGION_CODE;
 import static com.eventitta.user.exception.UserErrorCode.NOT_FOUND_USER_ID;
 
@@ -35,5 +39,18 @@ public class PostService {
             region
         );
         return postRepository.save(post).getId();
+    }
+
+    public void update(Long postId, Long userId, UpdatePostRequest request) {
+        Post post = postRepository.findById(postId)
+            .orElseThrow(NOT_FOUND_POST_ID::defaultException);
+
+        if (!post.getUser().getId().equals(userId)) {
+            throw ACCESS_DENIED.defaultException();
+        }
+        Region region = regionRepository.findById(request.regionCode())
+            .orElseThrow(RegionErrorCode.NOT_FOUND_REGION_CODE::defaultException);
+
+        post.update(request.title(), request.content(), region);
     }
 }

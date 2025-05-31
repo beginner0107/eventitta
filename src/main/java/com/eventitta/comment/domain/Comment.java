c6650e0710cad9d56d4f76e97635bab27dc22c20
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-06-01 00:12:54 +0900

    feat(comment): 지역 기반 게시글 댓글 엔티티 설계

---
+++ src/main/java/com/eventitta/comment/domain/Comment.java
@@ -0,0 +1,41 @@
+package com.eventitta.comment.domain;
+
+import com.eventitta.common.config.BaseEntity;
+import com.eventitta.post.domain.Post;
+import com.eventitta.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comments")
+public class Comment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private Comment parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> children = new ArrayList<>();
+}

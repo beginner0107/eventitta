f0bc961f2d52cb5e8da7b517ee0d7f6a79b8b86a
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-06-01 17:56:13 +0900

    feat(comment): CommentUpdateRequestDto 스웨거 설정 및 유효성 검증 메시지 추가

--- src/main/java/com/eventitta/comment/dto/request/CommentUpdateRequestDto.java
+++ src/main/java/com/eventitta/comment/dto/request/CommentUpdateRequestDto.java
@@ -2,7 +2,10 @@ package com.eventitta.comment.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
+import static com.eventitta.common.constants.ValidationMessage.COMMENT_CONTENT;
+
 public record CommentUpdateRequestDto(
-    @NotBlank String content
+    @NotBlank(message = COMMENT_CONTENT)
+    String content
 ) {
 }

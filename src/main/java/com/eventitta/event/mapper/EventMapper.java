3c46cb9b40c51de42f04243024df6c891b89e568
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-06-12 00:03:15 +0900

    feat(event): EventMapper 인터페이스 생성
    - @Mapper(componentModel="spring") EventMapper 작성

---
+++ src/main/java/com/eventitta/event/mapper/EventMapper.java
@@ -0,0 +1,23 @@
+package com.eventitta.event.mapper;
+
+import com.eventitta.event.dto.response.EventDistanceDto;
+import com.eventitta.event.dto.response.EventResponseDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface EventMapper {
+    EventMapper INSTANCE = Mappers.getMapper(EventMapper.class);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "place", target = "place")
+    @Mapping(source = "startTime", target = "startTime")
+    @Mapping(source = "endTime", target = "endTime")
+    @Mapping(source = "category", target = "category")
+    @Mapping(source = "isFree", target = "isFree")
+    @Mapping(source = "homepageUrl", target = "homepageUrl")
+    @Mapping(source = "distance", target = "distance")
+    EventResponseDto toResponse(EventDistanceDto src);
+}

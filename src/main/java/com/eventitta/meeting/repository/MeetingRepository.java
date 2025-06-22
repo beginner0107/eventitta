094fe49e837f157daed8dea299b5af811bbc0c86
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-06-22 21:45:30 +0900

    refactor(meeting): 사용하지 않는 메서드 삭제

--- src/main/java/com/eventitta/meeting/repository/MeetingRepository.java
+++ src/main/java/com/eventitta/meeting/repository/MeetingRepository.java
@@ -8,12 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 public interface MeetingRepository extends JpaRepository<Meeting, Long>, MeetingRepositoryCustom {
-
-    @Query("SELECT m FROM Meeting m WHERE m.id = :id AND m.deleted = false")
-    Optional<Meeting> findByIdAndDeletedFalse(@Param("id") Long id);
 
     @Modifying
     @Query("UPDATE Meeting m SET m.status = :status WHERE m.endTime <= :now AND m.status <> :status AND m.deleted = false")

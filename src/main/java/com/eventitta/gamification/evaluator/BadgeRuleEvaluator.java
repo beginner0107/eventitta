f24fb808fc119319fd43e4917e82f69141d6c96f
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-07-07 00:32:40 +0900

    remove(gamification): 이벤트 기반 비동기 방식으로 변경(json 방식 사용 안 함)

--- src/main/java/com/eventitta/gamification/evaluator/BadgeRuleEvaluator.java
+++
@@ -1,7 +0,0 @@
-package com.eventitta.gamification.evaluator;
-
-import com.eventitta.user.domain.User;
-
-public interface BadgeRuleEvaluator {
-    boolean evaluate(User user, String conditionJson);
-}

252546dc728149251ebf629ee225eef0c58914f1
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-07-27 22:29:00 +0900

    chore: Slf4j 어노테이션 추가 및 로그 기능 적용

--- src/main/java/com/eventitta/gamification/activitylog/ActivityEventPublisher.java
+++ src/main/java/com/eventitta/gamification/activitylog/ActivityEventPublisher.java
@@ -1,9 +1,11 @@
 package com.eventitta.gamification.activitylog;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ActivityEventPublisher {

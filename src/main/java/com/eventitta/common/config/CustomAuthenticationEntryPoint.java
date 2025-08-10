5c59b76a911d0a59d4217abc85a1a9386ec0fe95
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-08-11 00:01:24 +0900

    feat(auth): 인가 예외 처리 응답에 대해 UTF-8 인코딩

--- src/main/java/com/eventitta/common/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/eventitta/common/config/CustomAuthenticationEntryPoint.java
@@ -31,6 +31,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
         );
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
         response.getWriter().write(objectMapper.writeValueAsString(body));
     }
 }

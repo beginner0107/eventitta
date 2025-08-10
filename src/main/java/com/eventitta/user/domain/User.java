68b32fdea63a9ef8226a2debe5efb21b0674253f
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-08-10 14:49:41 +0900

    fix(user): JPA ddl-auto를 create-drop으로 업데이트하고 User 엔티티 pk 설정 변경
    - application-local.yml 파일에서 JPA ddl-auto 설정을 'update'에서 'create-drop'로 변경하여 시작 시 스키마 실행되도록 변경
    - User 엔티티의 id 필드에서 columnDefinition을 제거하여 엔티티 정의를 수정하여 자료형 크기를 단순화 시킴

--- src/main/java/com/eventitta/user/domain/User.java
+++ src/main/java/com/eventitta/user/domain/User.java
@@ -30,7 +30,6 @@ public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(columnDefinition = "INT UNSIGNED")
     private Long id;
 
     @Email

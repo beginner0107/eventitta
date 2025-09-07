61a7003e9db0bd6c71c1a56b9d6fb5bb78ff0c74
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-09-07 22:39:48 +0900

    feat(post): 게시물 삭제 후 이미지를 삭제하기 위해 추가

---
+++ src/main/java/com/eventitta/post/event/PostDeleteEventListener.java
@@ -0,0 +1,31 @@
+package com.eventitta.post.event;
+
+import com.eventitta.file.service.FileStorageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Slf4j
+@Component
+public class PostDeleteEventListener {
+
+    private final FileStorageService fileStorageService;
+
+    public PostDeleteEventListener(
+        @Qualifier(value = "localFileStorageService") FileStorageService fileStorageService) {
+        this.fileStorageService = fileStorageService;
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void onPostDeleted(PostDeletedEvent event) {
+        for (String url : event.getImageUrls()) {
+            try {
+                fileStorageService.delete(url);
+            } catch (Exception e) {
+                log.error("파일 삭제 실패: {}", url, e);
+            }
+        }
+    }
+}

f38868844e162e34aecde3f9783b7a7eb919bb3c
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-11-23 13:49:06 +0900

    feat: 포인트 기반 Badge 평가 기능(ActivityPointsRuleEvaluator) 도입
    - ActivityPointsRuleEvaluator를 도입하여 총 포인트 기반의 BadgeRule 평가를 지원
    - BadgeRuleEvaluator의 isSatisfied 의 매개변수를 새로 추가
    - 관련 DTO 및 Projection 수정

--- src/main/java/com/eventitta/gamification/evaluator/BadgeRuleEvaluator.java
+++ src/main/java/com/eventitta/gamification/evaluator/BadgeRuleEvaluator.java
@@ -1,10 +1,13 @@
 package com.eventitta.gamification.evaluator;
 
+import com.eventitta.gamification.domain.ActivityType;
 import com.eventitta.gamification.domain.BadgeRule;
 import com.eventitta.user.domain.User;
+
+import java.util.Map;
 
 public interface BadgeRuleEvaluator {
     boolean supports(BadgeRule rule);
 
-    boolean isSatisfied(User user, BadgeRule rule);
+    boolean isSatisfied(User user, BadgeRule rule, Map<ActivityType, Long> activityCountMap, Map<ActivityType, Long> activityPointsMap);
 }

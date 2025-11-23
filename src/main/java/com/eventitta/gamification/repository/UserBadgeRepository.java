d5d5c5dc5510109154f6c0c56fe320264da08782
Beginner <81161819+beginner0107@users.noreply.github.com>
2025-11-23 14:04:21 +0900

    feat: BadgeService를 리팩토링하기 위한 필요한 메서드 추가
    - BadgeRuleRepository에서 findAllEnabledWithBadge를 통해 활성화된 BadgeRule을 조회하는 메서드 추가
    - UserBadgeRepository에서 findBadgeIdsByUserId를 통해 단일 회원이 보유하고 있는 뱃지를 조회하는 메서드 추가

--- src/main/java/com/eventitta/gamification/repository/UserBadgeRepository.java
+++ src/main/java/com/eventitta/gamification/repository/UserBadgeRepository.java
@@ -2,7 +2,14 @@ package com.eventitta.gamification.repository;
 
 import com.eventitta.gamification.domain.UserBadge;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Set;
 
 public interface UserBadgeRepository extends JpaRepository<UserBadge, Long> {
     boolean existsByUserIdAndBadgeId(Long userId, Long badgeId);
+
+    @Query("SELECT ub.badge.id FROM UserBadge ub WHERE ub.user.id = :userId")
+    Set<Long> findBadgeIdsByUserId(@Param("userId") Long userId);
 }

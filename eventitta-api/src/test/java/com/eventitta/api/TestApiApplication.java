0f5761c568307de8bb6a16be773c38da4d790592
Beginner <81161819+beginner0107@users.noreply.github.com>
2026-04-10 13:15:31 +0900

    test: stabilize modular test bootstrap

---
+++ eventitta-api/src/test/java/com/eventitta/api/TestApiApplication.java
@@ -0,0 +1,7 @@
+package com.eventitta.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.eventitta.api")
+public class TestApiApplication {
+}
